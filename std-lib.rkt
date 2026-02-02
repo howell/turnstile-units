08629e7cc49d191dd08e62aad56881e76c40d802
Sam Caldwell <samcaldwell19@gmail.com>
2026-02-01 19:04:34 -0500

    add define-prefixed-unit

--- std-lib.rkt
+++ std-lib.rkt
@@ -2,14 +2,35 @@
 
 (provide (all-defined-out))
 
-(require "types.rkt")
+(require "types.rkt"
+         syntax/parse/define
+         (for-syntax syntax/stx
+                     racket/syntax
+                     racket/list))
+
+(begin-for-syntax
+  (define PREFIXES '((Nano 10.0e-9)
+                     (Micro 10.0e-6)
+                     (Milli 10.0e-3)
+                     (Centi 10.0e-2)
+                     (Deci 10.0e-1)
+                     (Kilo 10.0e3)
+                     (Mega 10.0e6)
+                     (Giga 10.0e9))))
+
+(define-syntax-parse-rule (define-prefixed-unit U:id (~datum :) M:id)
+  #:with ((prefixed-name factor) ...) (stx-map (lambda (p) (list (format-id #'U "~a~a" (first p) #'U)
+                                                                 (datum->syntax #'U (second p))))
+                                               PREFIXES)
+  (begin
+    (define-unit U : M)
+    (define-unit prefixed-name = factor * U)
+    ...))
 
 (define-measure Distance)
 (define-measure Time)
 
-(define-unit Meters : Distance)
-
-(define-unit Kilometers = 1000.0 * Meters)
+(define-prefixed-unit Meters : Distance)
 
 (define-unit Feet = 0.3048 * Meters)
 
@@ -17,7 +38,7 @@
 
 (define-unit Inches = 1/12 * Feet)
 
-(define-unit Seconds : Time)
+(define-prefixed-unit Seconds : Time)
 
 (define-unit Minutes = 60.0 * Seconds)
 
